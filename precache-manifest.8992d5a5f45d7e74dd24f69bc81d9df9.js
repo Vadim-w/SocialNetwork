bcab6fa456db55b42244a2d3dd010d9cc691b738
Vadim-w <vadim.morozov.2019@bk.ru>
2020-10-05 19:25:42 +0300

    Updates

---
+++ precache-manifest.8992d5a5f45d7e74dd24f69bc81d9df9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b966861084ab5e80d63a14639a050fe6",
+    "url": "/SocialNetwork/index.html"
+  },
+  {
+    "revision": "6e5e3792241090c3d301",
+    "url": "/SocialNetwork/static/css/main.6eb3a5d5.chunk.css"
+  },
+  {
+    "revision": "369ce52e559ff608d6c2",
+    "url": "/SocialNetwork/static/js/2.01cd314a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SocialNetwork/static/js/2.01cd314a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e5e3792241090c3d301",
+    "url": "/SocialNetwork/static/js/main.e6ef9a70.chunk.js"
+  },
+  {
+    "revision": "e97be8b6921101aba1cc",
+    "url": "/SocialNetwork/static/js/runtime-main.b8577c6e.js"
+  }
+]);
